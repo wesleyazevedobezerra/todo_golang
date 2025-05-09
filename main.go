a9bfd360bb6fa8825164eba227c7a75f223b4251
wesleyazevedobezerra <wesleyazevedobezerra@gmail.com>
2025-05-09 19:03:25 -0300

    Alteração remoção string duplicada 2

--- main.go
+++ main.go
@@ -7,32 +7,18 @@ import (
 )
 
 func main() {
-	// Conectar ao banco de dados
 	database.Connect()
 	r := gin.Default()
-
-	// Definindo as rotas
-	tasks := "/tasks"       // Rota para todas as tarefas
-	tasksId := "/tasks/:id" // Rota para tarefa específica com ID
+	taskId := "/tasks/:id"
 
 	api := r.Group("/api")
 	{
-		// Rota para obter todas as tarefas
-		api.GET(tasks, controllers.GetTasks)
-
-		// Rota para obter uma tarefa específica
-		api.GET(tasksId, controllers.GetTaskByID)
-
-		// Rota para criar uma nova tarefa
-		api.POST(tasks, controllers.CreateTask)
-
-		// Rota para atualizar uma tarefa específica
-		api.PUT(tasksId, controllers.UpdateTask)
-
-		// Rota para deletar uma tarefa específica
-		api.DELETE(tasksId, controllers.DeleteTask)
+		api.GET("/tasks", controllers.GetTasks)
+		api.GET(taskId, controllers.GetTaskByID)
+		api.POST("/tasks", controllers.CreateTask)
+		api.PUT(taskId, controllers.UpdateTask)
+		api.DELETE(taskId, controllers.DeleteTask)
 	}
 
-	// Iniciar o servidor
 	r.Run(":8080") // Inicia em http://localhost:8080
 }
